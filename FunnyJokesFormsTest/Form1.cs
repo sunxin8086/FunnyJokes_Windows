3fee51d46e255d92bda3c08d5d387046bc47bc25
Xin Sun <sunxin8086@gmail.com>
2013-08-21 00:07:30 -0700

    Add a form application to test PCL project.

---
+++ FunnyJokesFormsTest/Form1.cs
@@ -0,0 +1,23 @@
+﻿using FunnyJokesPortableClassLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunnyJokesFormsTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FunnyJokesRestDataService s = new FunnyJokesRestDataService();
+            s.getJokesByCategory("");
+        }
+    }
+}
