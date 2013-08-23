f3f4ece5938e96f2bc0376357581b17de35eb790
Xin Sun <sunxin8086@gmail.com>
2013-08-23 02:10:03 -0700

    Move the MainPage to Views folder

---
+++ WP8/Views/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using FunnyJokesPortableClassLibrary.Services;
+using System.Collections.ObjectModel;
+using FunnyJokesPortableClassLibrary.Contracts.Models;
+using Microsoft.Practices.ServiceLocation;
+using GalaSoft.MvvmLight.Ioc;
+using FunnyJokesPortableClassLibrary.Contracts.Services;
+using WP8.ViewModels;
+using WP8.Services;
+
+namespace WP8.Views
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+         
+            test();
+        }
+
+        public async void test()
+        {
+            FunnyJokesRestDataService s = new FunnyJokesRestDataService();
+            ObservableCollection<IJoke> jokes = await s.getJokesByCategory("adu_eng", 0, 2);
+        }
+    }
+}
