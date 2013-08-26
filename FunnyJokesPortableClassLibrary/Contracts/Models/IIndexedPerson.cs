074f27ac004118ab297023c22849f6d2b7b041bd
Xin Sun <sunxin8086@gmail.com>
2013-08-25 20:56:12 -0700

    Add windows phone control toolkit

---
+++ FunnyJokesPortableClassLibrary/Contracts/Models/IIndexedPerson.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FunnyJokesPortableClassLibrary.Contracts.Models
+{
+    public interface IIndexedPerson : IPerson
+    {
+        int Index { set; get; }
+    }
+}
---
+++ FunnyJokesPortableClassLibrary/Models/IndexedPerson.cs
@@ -0,0 +1,35 @@
+﻿using FunnyJokesPortableClassLibrary.Contracts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FunnyJokesPortableClassLibrary.Models
+{
+    public class IndexedPerson : Person, IIndexedPerson
+    {
+        public IndexedPerson(IPerson p, int index)
+        {
+            this.Username = p.Username;
+            this.JokesNumber = p.JokesNumber;
+            this.CommentsNumber = p.CommentsNumber;
+            this.LikesNumber = p.LikesNumber;
+            this.DislikesNumber = p.DislikesNumber;
+            if (String.IsNullOrEmpty(p.ProfileTitle) && String.IsNullOrEmpty(p.ProfileContent))
+                this.ProfileTitle = "He is too lazy to write anyting.";
+            else
+                this.ProfileTitle = String.IsNullOrEmpty(p.ProfileTitle) ? p.ProfileContent : p.ProfileTitle;
+            this.ProfileContent = p.ProfileContent;
+            this.Created = p.Created;
+            this.Updated = p.Updated;
+            this.Index = index;
+        }
+
+        private int index = 0;
+        public int Index
+        {
+            get { return index; }
+            set { Set(ref index, value); }
+        }
+    }
+}
