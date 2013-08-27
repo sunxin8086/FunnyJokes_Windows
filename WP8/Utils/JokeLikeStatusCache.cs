e87b5864f9720dab6281f5caa67c17a005011841
Xin Sun <sunxin8086@gmail.com>
2013-08-27 01:13:35 -0700

    Add the template cache class.

---
+++ WP8/Utils/JokeLikeStatusCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WP8.Utils
+{
+    public class JokeLikeStatusCache
+    {
+        private IDictionary<string, bool> likeStatusCache = new Dictionary<string, bool>();
+
+ 
+    }
+}
