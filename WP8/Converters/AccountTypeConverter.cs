ce1ec0ec04021fed8752a3f51b4d23c855650844
Xin Sun <sunxin8086@gmail.com>
2013-08-27 01:13:22 -0700

    Add new converters.

---
+++ WP8/Converters/AccountTypeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace FunnyJokes.Converters
+{
+    public class AccountTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string AccountType = (string)value;
+            string retVal = "Unknown";
+            switch (AccountType)
+            {
+                case "R": retVal = "Read Only"; break;
+                case "W": retVal = "Read & Write"; break;
+                case "M": retVal = "Moderator"; break;
+                case "A": retVal = "Administrator"; break;
+            }
+            return retVal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WP8/Converters/SexConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace FunnyJokes.Converters
+{
+    public class SexConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            bool sex = (bool)value;
+            return sex ? "Male" : "Female";
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            string sex = (string)value;
+            bool profileSex = true;
+            if(sex == "Female")
+                profileSex = false;
+            return profileSex;
+        }
+    }
+}
